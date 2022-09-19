b3fc64e0f7684391cd284d0a247142ff658b6c17
Nathan Graule <me@solarliner.dev>
2022-09-19 15:31:03 +0200

    chore: improve build chunks

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,25 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import {defineConfig} from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      ["onnxruntime-web"]: "onnxruntime-web/dist/ort.es6.min.js",
+      ['onnxruntime-web']: 'onnxruntime-web/dist/ort.es6.min.js',
     },
   },
-  base: '/latentwt'
+  base: '/latentwt/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('node_modules')) {
+            if (['onnxruntime-web', '@vue'].some(h => id.includes(h))) return 'runtime';
+            if (['chart.js', 'vue-chartjs', 'naive-ui'].some(h => h.includes(h))) return 'ui';
+          }
+        },
+      },
+    },
+  },
 });
